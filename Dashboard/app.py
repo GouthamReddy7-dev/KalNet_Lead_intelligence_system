3b43692f94541471983b794ca17a31e70a880481
GouthamReddy7-dev <gouthamreddy7272@gmail.com>
2026-05-15 18:20:20 +0530

    Update app.py

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -16,7 +16,7 @@ with st.form(key="DataForm"):
 
     state=st.selectbox(
         "State",
-        [None,'Telangana','Maharashtra','Andhra Pradesh','Tamil Nadu']
+        [None,'Telangana','Maharashtra','Delhi','Tamil Nadu','Karnataka']
     )
 
     school_type=st.selectbox(
